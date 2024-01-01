1cc1193ceaae2d6e4f5b94c9d88fda0b26ff732e
Isaac Akin-Onigbinde <aakinonigbinde@gmail.com>
2024-01-01 22:18:36 +0000

    baselines

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
-from langchain.prompts import PromptTemplate
+# from langchain.prompts import PromptTemplate
 # from langchain.chains import LLMChain, SimpleSequentialChain, SequentialChain
 # from langchain.memory import ConversationBufferMemory
 # from langchain.utilities import WikipediaAPIWrapper
@@ -12,8 +12,6 @@ from langchain.prompts.chat import HumanMessagePromptTemplate, ChatPromptTemplat
 from pydantic import BaseModel, Field
 from langchain.output_parsers import PydanticOutputParser
 
-from langchain.chains.api import open_meteo_docs
-from langchain.chains import APIChain
 
 
 class Country(BaseModel):
@@ -21,30 +19,34 @@ class Country(BaseModel):
     name: str = Field(description="name of the country")
 
 load_dotenv()
-
-
-
 OPENAI_MODEL = "gpt-3.5-turbo"
 # OPENAI_MODEL = "gpt-4"
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
-PROMPT_COUNTRY_INFO = """Provide information about {country}.{format_instructions}. If the country is doesn't exist, make up a response."""
+PROMPT_COUNTRY_INFO = """
+Provide information about {country}.{format_instructions}. 
+If the country is doesn't exist, make up a response.
+"""
 
 # using prompt and chat llm model 
 def main():
+    # V1
     # llm = OpenAI(openai_api_key=OPENAI_API_KEY)
     llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY, model_name=OPENAI_MODEL)
+    # for stucturing LLM output 
     parser = PydanticOutputParser(pydantic_object=Country)
     country = input("Enter the name of a country: ")
-    
+    # core 
     message = HumanMessagePromptTemplate.from_template(template=PROMPT_COUNTRY_INFO)
     chat_prompt = ChatPromptTemplate.from_messages(messages=[message])
     chat_prompt_with_values = chat_prompt.format_prompt(country=country, format_instructions=parser.get_format_instructions())
     try:
+        # V1 
         # result = llm.predict(
         #     "Give me 5 topics for interesting YouTube videos about Python"
         #     )
         # print(result)
         response = llm(chat_prompt_with_values.to_messages())
+        # V2
         # print(response)
         data = parser.parse(response.content)
         # print(data)
---
+++ source/sysclass.py
@@ -0,0 +1,6 @@
+
+
+
+class Rawtweet():
+    def __init__(self) -> None:
+        self.id = self.generate_id()
